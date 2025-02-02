bbccd945463ca842ffe82eb3ff96f10a9dc3de93
devitalo97 <dev.italo.souza@gmail.com>
2025-02-02 19:22:11 -0300

    feat: add util function

---
+++ lib/components/hooks/use-update-url.tsx
@@ -0,0 +1,31 @@
+import { useRouter, usePathname, useSearchParams } from "next/navigation";
+
+export function useUpdateUrl() {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const updateUrl = (params: Record<string, any | null>, replace = false) => {
+    const currentParams = new URLSearchParams(searchParams.toString());
+
+    Object.entries(params).forEach(([key, value]) => {
+        if (Array.isArray(value)) {
+            value.forEach((v) => params.append(key, v)); 
+        } else if (value === null) {
+            currentParams.delete(key); 
+        } else {
+            currentParams.set(key, value); 
+        }
+    });
+
+    const newUrl = `${pathname}?${currentParams.toString()}`;
+
+    if (replace) {
+      router.replace(newUrl); // Altera a URL sem adicionar histórico (substitui)
+    } else {
+      router.push(newUrl); // Adiciona ao histórico do navegador
+    }
+  };
+
+  return updateUrl;
+}
