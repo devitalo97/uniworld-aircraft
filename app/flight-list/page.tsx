854ab17cbdd5d0ca1013a4801374d47580c8977b
devitalo97 <dev.italo.souza@gmail.com>
2025-02-02 19:28:35 -0300

    feat: implement page to search flight on App Router

--- app/flight-list/page.tsx
+++ app/flight-list/page.tsx
@@ -1,25 +1,28 @@
 import { fetchFlightList } from "@/lib/action";
 import { getCurrentDateInterval } from "@/lib/utils";
-import { FlightListTable } from "@/lib/components/tables";
+import { FlightTable } from "@/lib/components/tables";
 import { PageHeader } from "@/lib/components/page-header";
 
 export const revalidate = 60
 
-export default async function LightList() {
+export default async function FlightList() {
   const { start, end } = getCurrentDateInterval()
   const flightList = await fetchFlightList({
-    start,
-    end
+    startTimeInterval: start,
+    endTimeInterval: end,
+    flightType: [],
+    flightStatus: [],
+    aircraftNidList: []
   });
 
   return (
     <>
       <div className="px-4 py-10 sm:px-6 lg:px-8">
-       <PageHeader />
+       <PageHeader withTime={true}/>
       </div>
 
       <div className="px-4 pb-10 sm:px-6 lg:px-8">
-        <FlightListTable flightList={flightList}/>
+        <FlightTable flightList={flightList}/>
       </div>
     </>
   );
---
+++ app/flight-list/search/page.tsx
@@ -0,0 +1,51 @@
+import { fetchFlightList } from "@/lib/action";
+import { getCurrentDateInterval } from "@/lib/utils";
+import { FlightTable } from "@/lib/components/tables";
+import { PageHeader } from "@/lib/components/page-header";
+import { FlightSearchForm } from "@/lib/components/forms/flight.search.form";
+
+interface Params {
+  searchParams: {
+    startTimeInterval: string 
+    endTimeInterval: string
+    flightType: string[]
+    flightStatus: string[]
+    aircraftNidList: string[]
+  }
+}
+
+export const revalidate = 60
+
+export default async function FlightListWithSearch(params: Params) {
+  const { start, end } = getCurrentDateInterval()
+  const {
+    searchParams: {
+      startTimeInterval,
+      endTimeInterval,
+    }
+  } = params
+
+  const flightList = await fetchFlightList({
+    startTimeInterval: startTimeInterval ?? start,
+    endTimeInterval: endTimeInterval ?? end,
+    flightType: [],
+    flightStatus: [],
+    aircraftNidList: []
+  });
+
+  return (
+    <>
+      <div className="px-4 py-10 sm:px-6 lg:px-8">
+       <PageHeader  withTime={false}/>
+      </div>
+
+      <div className="px-4 pb-10 sm:px-6 lg:px-8">
+       <FlightSearchForm />
+      </div>
+
+      <div className="px-4 pb-10 sm:px-6 lg:px-8">
+        <FlightTable flightList={flightList}/>
+      </div>
+    </>
+  );
+}
