5a66f314e99c71027a980420be45ee6e9a064132
devitalo97 <dev.italo.souza@gmail.com>
2025-02-02 16:35:47 -0300

    refactor: modify 'formatDate' function to allows use multiple locales and options

--- lib/utils/format-date.ts
+++ lib/utils/format-date.ts
@@ -1,12 +1,25 @@
-export function formatDateToEN(data: string | number) {
-    const date = new Date(data);
-    return date.toLocaleString("en-US", {
-      year: "2-digit",
-      month: "2-digit",
-      day: "2-digit",
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-      hour12: true,
-    });
-  }
+export function formatDate(input: {
+  date: string | number,
+  locales?: Intl.LocalesArgument, 
+  options?: Intl.DateTimeFormatOptions
+}) {
+  const { date, locales = "en-US", options = {} } = input;
+  const dateObj = new Date(date);
+
+  // Definição de opções padrão
+  const defaultOptions: Intl.DateTimeFormatOptions = {
+    year: "2-digit",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true,
+    timeZone: "UTC" // Define UTC como padrão
+  };
+
+  // Mescla as opções padrão com as passadas pelo usuário
+  const finalOptions = { ...defaultOptions, ...options };
+
+  return dateObj.toLocaleString(locales, finalOptions);
+}
