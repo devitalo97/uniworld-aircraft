82d08b2ef5c4c388c5d1b8f52d9c586aa21ae73f
devitalo97 <dev.italo.souza@gmail.com>
2025-02-06 19:26:28 -0300

    feat: redirect from '/' to 'dashboard'

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    redirects: async () => [
+        {
+          source: "/",
+          destination: "/dashboard",
+          permanent: true, // ou false, se quiser um redirecionamento temporário
+        },
+      ],
+};
 
 export default nextConfig;
