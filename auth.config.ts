b3b058d8df07665cf0d9645435872901add97908
devitalo97 <dev.italo.souza@gmail.com>
2025-04-28 20:22:47 -0300

    chore: add new logo asset

--- auth.config.ts
+++ auth.config.ts
@@ -1,29 +1,37 @@
 import type { NextAuthConfig } from "next-auth";
-import Credentials from 'next-auth/providers/credentials';
+import Credentials from "next-auth/providers/credentials";
 
 export const authConfig = {
   pages: {
-    signIn: '/login',
+    signIn: "/login",
   },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
+      if (nextUrl.pathname.match(/\.(svg|png|jpg|css|js|ico|jpeg)$/)) {
+        return true;
+      }
       const isLoggedIn = !!auth?.user;
-      const isProtectedRoute = ["/", "/dashboard", "/flight", "/configuration"].some(route => {
-        if(route === "/"){
-            return nextUrl.pathname === route
-        }else{
-            return nextUrl.pathname.includes(route)
+      const isProtectedRoute = [
+        "/",
+        "/dashboard",
+        "/flight",
+        "/configuration",
+      ].some((route) => {
+        if (route === "/") {
+          return nextUrl.pathname === route;
+        } else {
+          return nextUrl.pathname.includes(route);
         }
       });
-      
+
       if (isProtectedRoute) {
         return isLoggedIn;
       }
-      
+
       if (isLoggedIn) {
-        return Response.redirect(new URL('/dashboard', nextUrl));
+        return Response.redirect(new URL("/dashboard", nextUrl));
       }
-      
+
       return true;
     },
     jwt({ user, token }) {
@@ -36,7 +44,7 @@ export const authConfig = {
       session.user.id = String(token.id);
       session.user.role = token.role as "user" | "admin" | "moderator";
       return session;
-    }
+    },
   },
   providers: [Credentials({})],
 } satisfies NextAuthConfig;
--- lib/components/page-header.tsx
+++ lib/components/page-header.tsx
@@ -1,10 +1,12 @@
+"use client";
+
 export function PageHeader() {
   return (
     <div className="mx-auto flex items-center justify-between gap-x-8 lg:mx-0">
       <div className="flex items-center gap-x-6">
         <img
           alt="Uniworld Logo"
-          src="/uniworld_logo.png"
+          src="/uniworld.jpeg"
           className="size-16 flex-none rounded-full ring-1 ring-gray-900/10 dark:bg-slate-50"
         />
         <h1>
