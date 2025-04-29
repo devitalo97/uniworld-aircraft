c939dcc8c9cceda30ab72bf32d1a864a0365ba46
devitalo97 <dev.italo.souza@gmail.com>
2025-04-28 22:10:24 -0300

    style: logo

--- lib/components/page-header.tsx
+++ lib/components/page-header.tsx
@@ -7,7 +7,7 @@ export function PageHeader() {
         <img
           alt="Uniworld Logo"
           src="/uniworld.jpeg"
-          className="size-16 flex-none rounded-full ring-1 ring-gray-900/10 dark:bg-slate-50"
+          className="size-16 flex-none rounded-md ring-1 ring-gray-900/10 dark:bg-slate-50"
         />
         <h1>
           <div className="text-sm/6 text-gray-500 dark:text-gray-400">
